4ff0b21ac9dfade682b8ab7a1a4b7d87584744b1
Porpor556 <nrwathnthxngheluxng@gmail.com>
2025-08-29 12:24:02 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker ติดตั้งแล้ว");
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("app-cache").then(cache => {
+      return cache.addAll(["/index.html"]);
+    })
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
